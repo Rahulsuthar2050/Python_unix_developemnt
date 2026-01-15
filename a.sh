cc4c2a4299fd941c9b5cb1761564543a0a583e45
deepeshsuthar46 <deepesh/login-com>
2026-01-15 09:14:12 +0530

    A.sh is woring with addtion

--- a.sh
+++ a.sh
@@ -2,9 +2,9 @@ echo "My name is Rahul"
 echo "This is for practice of unix and python reportiory"
 echo "$date"
 
-a = "5"
+a=5
 echo "Vaube of b is $a"
-b = "10"
+b=10
 echo "value of a is $b"
-c = a+b
+c=$(($a+$b))
 echo $c
